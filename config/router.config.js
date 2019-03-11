d638ecfdaf2c54273158bdb00584e7821fdec0a2
zhuleishan <Iciyun123>
2019-03-11 16:18:12 +0800

    回滚

--- config/router.config.js
+++ config/router.config.js
@@ -65,9 +65,30 @@ export default [
             component: './instrumentagents/CopingManagement/CopingList',
           },
           {
-            path: '/Instrumentagents/credentialsquery-list',
+            path: '/Instrumentagents/credentialsquery',
             name: 'credentialsquery',
             component: './Instrumentagents/CredentialsQuery',
+            hideChildrenInMenu: true,
+            routes: [
+              {
+                path: '/Instrumentagents/credentialsquery',
+                redirect: '/Instrumentagents/credentialsquery/index',
+              },
+              {
+                path: '/Instrumentagents/credentialsquery/index',
+                component: './Instrumentagents/CredentialsQuery/Step1',
+              },
+              {
+                path: '/Instrumentagents/credentialsquery/payand',
+                name: 'payand',
+                component: './Instrumentagents/CredentialsQuery/Step2',
+              },
+              {
+                path: '/Instrumentagents/credentialsquery/designs',
+                name: 'designs',
+                component: './Instrumentagents/CredentialsQuery/Step3',
+              },
+            ],
           },
         ],
       },
